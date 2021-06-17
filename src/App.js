3992afd0593023d2d6a2de21482b7f032ba93b3f
emilskovmand <emilskovmand@gmail.com>
2021-06-17 10:56:47 +0200

    Setup of react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+export default function App() {
+	return (
+		<>
+			<Router>
+
+
+				<Switch>
+					<Route exact path='/' >
+
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	)
+}
